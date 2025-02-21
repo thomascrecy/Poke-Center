23a138d6e3520bc29329b83aa9d01937f6108ced
Aube33 <ant.reberat@murena.io>
2025-02-21 16:48:17 +0100

    /sell fonctionnel

--- src/Controller/SellController.php
+++ src/Controller/SellController.php
@@ -2,17 +2,57 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Article;
+use App\Form\SellType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class SellController extends AbstractController
 {
     #[Route('/sell', name: 'app_sell')]
-    public function index(): Response
-    {
+    public function index(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        SluggerInterface $slugger
+    ): Response {
+        $newArticle = new Article();
+        $form = $this->createForm(SellType::class, $newArticle);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $imageFile = $form->get('imageFile')->getData();
+
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('images_directory'), // Défini dans services.yaml
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    throw new \Exception('Erreur lors de l\'upload de l\'image.');
+                }
+
+                $newArticle->setImageLink($newFilename); // Sauvegarde le chemin du fichier
+            }
+            $newArticle->setAuthorId($this->getUser());
+            $newArticle->setCreatedAt(new \DateTimeImmutable());
+
+            $entityManager->persist($newArticle);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_account');
+        }
+
         return $this->render('sell/index.html.twig', [
-            'controller_name' => 'SellController',
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/SellType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use App\Entity\Cart;
+use App\Entity\Stock;
+use App\Entity\Type;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SellType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('price')
+            ->add('imageFile', FileType::class, [
+                'label' => 'Image de l\'article',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/webp',
+                        ],
+                        'mimeTypesMessage' => 'Merci d\'uploader une image valide (JPEG, PNG ou WEBP)',
+                    ])
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
