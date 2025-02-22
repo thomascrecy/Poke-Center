efd7b6847cfc1bf23a82574d57bc0e2200f73b3b
Aube33 <ant.reberat@murena.io>
2025-02-22 22:22:10 +0100

    add /detail back

--- src/Controller/DetailController.php
+++ src/Controller/DetailController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class DetailController extends AbstractController
 {
-    #[Route('/detail', name: 'app_detail')]
-    public function index(): Response
+    #[Route('/detail/{id}', name: 'app_detail')]
+    public function index(int $id, EntityManagerInterface $em): Response
     {
+        $article = $em->getRepository(Article::class)->find($id);
+
+        if (!$article) {
+            throw $this->createNotFoundException('Article non trouvé');
+        }
+
         return $this->render('detail/index.html.twig', [
-            'controller_name' => 'DetailController',
+            'article' => $article,
         ]);
     }
 }
